c3ca3cabde58b8c74ec90cf8d27b2db66ba760b6
aestheticsdata <aesthetics.data@gmail.com>
2021-11-12 13:17:16 +0100

    fps selector

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ class Main {
   private _iterationCounterValue: number = 0
   private _iterationCounter: HTMLElement = document.querySelector('.iteration-counter')
   private _pauseBtn: HTMLButtonElement = document.querySelector('button')
+  private _speedSelector: HTMLInputElement = document.querySelector('#speed-input')
   private _fps = 7
   private _fpsInterval
   private _startTime
@@ -21,6 +22,22 @@ class Main {
     this._stage = this._canvas.getContext('2d')
     this._grid = new Grid(this._stage, this._canvas)
     this._pauseBtn.addEventListener('click', this._togglePause)
+    this._setFPS()
+  }
+
+  private _setFPS() {
+    this._speedSelector.value = String(this._fps)
+    this._speedSelector.addEventListener('keypress', this._handleKeyUp)
+    this._speedSelector.addEventListener('change', (e: Event) => {
+      this._fps = parseInt((e.currentTarget as HTMLInputElement).value)
+    })
+  }
+
+  private _handleKeyUp(e: KeyboardEvent) {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      (e.currentTarget as HTMLInputElement).blur();
+    }
   }
 
   private _renderGeneration() {
@@ -29,34 +46,34 @@ class Main {
 
   private _togglePause = () => {
     console.log('toggle pause')
-    this._isPlaying ? this._stop() : this._start();
-    this._isPlaying = !this._isPlaying;
+    this._isPlaying ? this._stop() : this._start()
+    this._isPlaying = !this._isPlaying
   }
 
   private _step = (_timestamp) => {
+    this._fpsInterval = 1000 / this._fps
     this._now = Date.now();
-    this._elapsed = this._now - this._lastDrawTime;
+    this._elapsed = this._now - this._lastDrawTime
     if (this._elapsed > this._fpsInterval) {
-      this._lastDrawTime = this._now - (this._elapsed % this._fpsInterval);
+      this._lastDrawTime = this._now - (this._elapsed % this._fpsInterval)
       this._iterationCounterValue++
       this._iterationCounter.textContent = String(this._iterationCounterValue)
-      this._renderGeneration();
+      this._renderGeneration()
     }
-    this._requestAnimationID = window.requestAnimationFrame(this._step);
+    this._requestAnimationID = window.requestAnimationFrame(this._step)
   }
 
   private _start = () => {
     console.log('start')
     // https://stackoverflow.com/questions/19764018/controlling-fps-with-requestanimationframe
-    this._fpsInterval = 1000 / this._fps;
-    this._lastDrawTime = Date.now();
-    this._startTime = this._lastDrawTime;
-    this._requestAnimationID = window.requestAnimationFrame(this._step);
+    this._lastDrawTime = Date.now()
+    this._startTime = this._lastDrawTime
+    this._requestAnimationID = window.requestAnimationFrame(this._step)
   }
 
   private _stop = () => {
-    console.log('stop');
-    cancelAnimationFrame(this._requestAnimationID);
+    console.log('stop')
+    cancelAnimationFrame(this._requestAnimationID)
   }
 
   init() {
