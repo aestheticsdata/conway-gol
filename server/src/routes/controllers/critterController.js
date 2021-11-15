ef11a25d3ed5b8eaf1060609f351fe945e0e7e5e
aestheticsdata <aesthetics.data@gmail.com>
2021-11-15 15:19:32 +0100

    server critter controler / nodemon

--- server/src/routes/controllers/critterController.js
+++ server/src/routes/controllers/critterController.js
@@ -1,7 +1,11 @@
+const fs = require('fs').promises;
+const path = require('path');
+
 module.exports = async (req, res, _next) => {
   try {
-    console.log('name', req.body.name)
-    res.json();
+    console.log('name', req.params.name)
+    const file = await fs.readFile(path.resolve(__dirname+`/../../species/${req.params.name}.hxf`), 'utf-8');
+    res.json(file);
   } catch (err) {
     console.log(err);
   }
